13d97d848dce2e4291b0ea12bc3769bffd7df5e2
iven ari <ivenari.dacruz@versia.com>
2021-11-16 11:37:19 +0100

    creamos la clase service que implementa de la interface repository

---
+++ src/main/java/com/ary/clientes/api/service/IClienteService.java
@@ -0,0 +1,11 @@
+package com.ary.clientes.api.service;
+
+import java.util.List;
+
+import com.ary.clientes.api.models.Cliente;
+
+public interface IClienteService {
+	
+	public List<Cliente> findAll();
+
+}
