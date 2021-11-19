89a8fd68860bbf18bf97a32248d2e4caddaacf2a
iven ari <ivenari.dacruz@versia.com>
2021-11-19 14:32:43 +0100

    actualizando los endpoint

--- src/main/java/com/ary/clientes/api/controllers/ClienteRestController.java
+++ src/main/java/com/ary/clientes/api/controllers/ClienteRestController.java
@@ -1,6 +1,7 @@
 package com.ary.clientes.api.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -9,9 +10,9 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,43 +20,52 @@ import com.ary.clientes.api.models.Cliente;
 import com.ary.clientes.api.service.ClienteService;
 
 //la ruta donde tenemos el frontend la damos permiso para conectar con el api-rest
-@CrossOrigin(origins = {"http://localhost:4200/"})
+@CrossOrigin(origins = { "http://localhost:4200/" })
 @RestController
 @RequestMapping("/api")
 public class ClienteRestController {
-	
+
 	@Autowired
 	private ClienteService clienteService;
-	
-	
-	
-	@GetMapping("/clientes")       //para ver todo el listado
+
+	@GetMapping("/clientes") // para ver todo el listado
 	public List<Cliente> listaClientes() {
-		
-		return ( List<Cliente> )clienteService.findAll();
+
+		return (List<Cliente>) clienteService.findAll();
 	}
-	
-	
-//	@GetMapping("/clientes/{id}")            
-//	public Cliente buscar(@PathVariable Long id) {  //buscar por id
-//		return clienteService.findById(id);
-//	}
-	
-	@PostMapping("/clientes")   //crear registro
+
+	@PostMapping("/clientes") // crear registro
 	@ResponseStatus(HttpStatus.CREATED)
 	public Cliente crear(@RequestBody Cliente cliente) {
 		return clienteService.save(cliente);
 	}
-	
-	
-	@DeleteMapping("/clientes/{id}")   //para eliminar un registro
+
+	@GetMapping("/clientes/{id}")
+	public Optional<Cliente> buscar(@PathVariable Long id) { // buscar por id
+		return clienteService.findById(id);
+	}
+
+	@DeleteMapping("/clientes/{id}") // para eliminar un registro
 	public void delete(@PathVariable Long id) {
 		clienteService.deleteById(id);
 	}
+
+	// creamos en endpoint actualizar un estudiante @requestBody
+	@PutMapping("/clientes/{id}")
+	public void actualizarEstudiante(@RequestBody Cliente cliente) {
+		clienteService.actualizarModificar(cliente);
+	}
 	
-	
-	//put
-	
+	@PutMapping("/clientes/{id}")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente update(@RequestBody Cliente cliente, @PathVariable Long id) {
+		Cliente currentCliente = this.clienteService.modificar(id);
+		currentCliente.setNombre(cliente.getNombre());
+		currentCliente.setApellido(cliente.getApellido());
+		currentCliente.setEmail(cliente.getEmail());
+		this.clienteService.save(currentCliente);
+		return currentCliente;
+	}
 	
 	
 
--- src/main/java/com/ary/clientes/api/service/ClienteService.java
+++ src/main/java/com/ary/clientes/api/service/ClienteService.java
@@ -1,55 +1,157 @@
 package com.ary.clientes.api.service;
 
+import java.util.List;
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.ary.clientes.api.models.Cliente;
 import com.ary.clientes.api.repositoryDao.IClienteRepository;
 
-
 @Service
-public class ClienteService implements IClienteRepository {
+public class ClienteService implements IClienteRepository, IClienteService{
+
 	
-	 @Autowired
-     private IClienteRepository iClienteRepository;
-	 
+	@Autowired
+	private IClienteService iClienteService;
+	
+	
+	@Autowired
+	private IClienteRepository iClienteRepository;
+	
 
 	@Override
-	public <S extends Cliente> S save(S entity) {  // para crear o guardar 
-		// TODO Auto-generated method stub
+	public <S extends Cliente> S save(S entity) { // para crear o guardar
+
 		return iClienteRepository.save(entity);
 	}
 
 	@Override
-	public <S extends Cliente> Iterable<S> saveAll(Iterable<S> entities) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	public List<Cliente> findAll() { // para ver toda la lista
 
-	@Override
-	public Optional<Cliente> findById(Long id) { // para buscar por id 
-		// TODO Auto-generated method stub
-		return iClienteRepository.findById(id);
-	}
-
-	@Override
-	public boolean existsById(Long id) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public Iterable<Cliente> findAll() {  // para ver toda la lista 
-		// TODO Auto-generated method stub
 		return iClienteRepository.findAll();
 	}
 
 	@Override
-	public Iterable<Cliente> findAllById(Iterable<Long> ids) {
+	public Optional<Cliente> findById(Long id) { // buscar un registro por id
+
+		return iClienteRepository.findById(id);
+	}
+
+	@Override
+	public void deleteById(Long id) { // para eliminar
+
+		iClienteRepository.deleteById(id);
+
+	}
+
+	// creamos el metodo manualmente para actualizar o modificar
+	public void actualizarModificar(Cliente cliente) {
+		iClienteRepository.save(cliente);
+	}
+	
+	
+	
+	
+	
+
+	@Override
+	public Cliente getOne(Long id) {
+
+		return null;
+	}
+
+	@Override
+	public List<Cliente> findAll(Sort sort) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public List<Cliente> findAllById(Iterable<Long> ids) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends Cliente> List<S> saveAll(Iterable<S> entities) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void flush() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public <S extends Cliente> S saveAndFlush(S entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends Cliente> List<S> saveAllAndFlush(Iterable<S> entities) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteAllInBatch(Iterable<Cliente> entities) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deleteAllByIdInBatch(Iterable<Long> ids) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deleteAllInBatch() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Cliente getById(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+
+	@Override
+	public <S extends Cliente> List<S> findAll(Example<S> example) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends Cliente> List<S> findAll(Example<S> example, Sort sort) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Page<Cliente> findAll(Pageable pageable) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean existsById(Long id) {
+		
+		return iClienteRepository.existsById(id);
 	}
 
 	@Override
@@ -59,36 +161,66 @@ public class ClienteService implements IClienteRepository {
 	}
 
 	@Override
-	public void deleteById(Long id) {   // para eliminar 
-		// TODO Auto-generated method stub
-		iClienteRepository.deleteById(id);
-		
-	}
-
-	@Override
 	public void delete(Cliente entity) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void deleteAllById(Iterable<? extends Long> ids) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void deleteAll(Iterable<? extends Cliente> entities) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void deleteAll() {
 		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public <S extends Cliente> Optional<S> findOne(Example<S> example) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends Cliente> Page<S> findAll(Example<S> example, Pageable pageable) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends Cliente> long count(Example<S> example) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public <S extends Cliente> boolean exists(Example<S> example) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	@Transactional()
+	public Cliente modificar(Long id) {
 		
+		return iClienteService.modificar(id);
 	}
 
 	
+	
+//	@Override
+//	public Cliente modificar(Long id) {
+//		
+//		return iClienteRepository.modificar(id);
+//	}
 
 }
