9163d834ece528f007bd4c545a9b95869a45e3fc
iven ari <ivenari.dacruz@versia.com>
2021-11-25 08:55:09 +0100

    se modifica el pom.xml y el cliente para no usar constructores y setter and getter

--- src/main/java/com/ary/clientes/api/models/Cliente.java
+++ src/main/java/com/ary/clientes/api/models/Cliente.java
@@ -16,7 +16,10 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
+import lombok.Data;
+
 @Entity
+@Data
 @Table(name = "clientes")
 public class Cliente implements Serializable {
 
@@ -46,113 +49,6 @@ public class Cliente implements Serializable {
 	}
 	
 	
-	
-	public Cliente() {
-		//super();
-		// TODO Auto-generated constructor stub
-	}
-	
-	
-
-
-
-	public Cliente(Long id, String nombre, String apellido, String email, Date fecha) {
-		//super();
-		this.id = id;
-		this.nombre = nombre;
-		this.apellido = apellido;
-		this.email = email;
-		this.fecha = fecha;
-	}
-	
-	
-	
-
-
-
-
-
-	public Long getId() {
-		return id;
-	}
-
-
-
-
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-
-
-
-
-	public String getNombre() {
-		return nombre;
-	}
-
-
-
-
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-
-
-
-
-	public String getApellido() {
-		return apellido;
-	}
-
-
-
-
-
-	public void setApellido(String apellido) {
-		this.apellido = apellido;
-	}
-
-
-
-
-
-	public String getEmail() {
-		return email;
-	}
-
-
-
-
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-
-
-
-
-	public Date getFecha() {
-		return fecha;
-	}
-
-
-
-
-
-	public void setFecha(Date fecha) {
-		this.fecha = fecha;
-	}
-
-
-
-
-
-
 
 
 	private static final long serialVersionUID = 1L;
