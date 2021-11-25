347a960f720d65465b2363d6c1d23b004b1d63ea
iven ari <ivenari.dacruz@versia.com>
2021-11-25 09:50:05 +0100

    borramos los metodos que no se usa en el service

--- src/main/java/com/ary/clientes/api/service/ClienteService.java
+++ src/main/java/com/ary/clientes/api/service/ClienteService.java
@@ -14,7 +14,7 @@ import com.ary.clientes.api.models.Cliente;
 import com.ary.clientes.api.repositoryDao.IClienteRepository;
 
 @Service
-public class ClienteService implements IClienteRepository{
+public class ClienteService{
 
 //	
 //	@Autowired
@@ -25,25 +25,25 @@ public class ClienteService implements IClienteRepository{
 	private IClienteRepository iClienteRepository;
 	
 
-	@Override
+
 	public <S extends Cliente> S save(S entity) { // para crear o guardar
 
 		return iClienteRepository.save(entity);
 	}
 
-	@Override
+
 	public List<Cliente> findAll() { // para ver toda la lista
 
 		return iClienteRepository.findAll();
 	}
 
-	@Override
+
 	public Optional<Cliente> findById(Long id) { // buscar un registro por id
 
 		return iClienteRepository.findById(id);
 	}
 
-	@Override
+
 	public void deleteById(Long id) { // para eliminar
 
 		iClienteRepository.deleteById(id);
@@ -56,155 +56,13 @@ public class ClienteService implements IClienteRepository{
 	}
 	
 	
-	
-	
-	
+//	@Override
+//	public boolean existsById(Long id) {
+//		
+//		return iClienteRepository.existsById(id);
+//	}
 
-	@Override
-	public Cliente getOne(Long id) {
 
-		return null;
-	}
-
-	@Override
-	public List<Cliente> findAll(Sort sort) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<Cliente> findAllById(Iterable<Long> ids) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public <S extends Cliente> List<S> saveAll(Iterable<S> entities) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void flush() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public <S extends Cliente> S saveAndFlush(S entity) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public <S extends Cliente> List<S> saveAllAndFlush(Iterable<S> entities) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void deleteAllInBatch(Iterable<Cliente> entities) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void deleteAllByIdInBatch(Iterable<Long> ids) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void deleteAllInBatch() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public Cliente getById(Long id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	
-
-	@Override
-	public <S extends Cliente> List<S> findAll(Example<S> example) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public <S extends Cliente> List<S> findAll(Example<S> example, Sort sort) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Page<Cliente> findAll(Pageable pageable) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public boolean existsById(Long id) {
-		
-		return iClienteRepository.existsById(id);
-	}
-
-	@Override
-	public long count() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public void delete(Cliente entity) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void deleteAllById(Iterable<? extends Long> ids) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void deleteAll(Iterable<? extends Cliente> entities) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void deleteAll() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public <S extends Cliente> Optional<S> findOne(Example<S> example) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public <S extends Cliente> Page<S> findAll(Example<S> example, Pageable pageable) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public <S extends Cliente> long count(Example<S> example) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public <S extends Cliente> boolean exists(Example<S> example) {
-		// TODO Auto-generated method stub
-		return false;
-	}
 
 //	@Override
 //	@Transactional()
